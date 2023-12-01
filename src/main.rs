7a552772cf7bbb75c48249265d4d3b5af6dbcf23
Andrew X. Shah <drewshah0@gmail.com>
2023-12-01 08:15:22 -0700

    add random car facts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use songbird::SerenityInit;
 
+use rand::{thread_rng, Rng};
 use serenity::async_trait;
 use serenity::builder::CreateMessage;
 use serenity::gateway::ActivityData;
@@ -10,7 +11,31 @@ use std::env;
 
 struct Handler;
 
+const BOT_ID: u64 = 1179957141688291498;
 const ADAM_ID: u64 = 281207443105644544;
+
+const CAR_FACTS: [&str; 20] = [
+    "The first recorded car accident occurred in 1891 in Ohio, USA, when a steam-powered vehicle collided with a tree.",
+    "The world's fastest production car is the Bugatti Chiron Super Sport 300+, reaching a top speed of 304 mph (490 km/h).",
+    "The average car has about 30,000 parts.",
+    "The first mass-produced car was the Model T by Ford, introduced in 1908.",
+    "The Volkswagen Beetle is one of the best-selling cars in history, with over 21 million units sold.",
+    "The first car with windshield wipers was the 1903 Cadillac Model A.",
+    "The longest-lasting car model still in production is the Chevrolet Suburban, introduced in 1935.",
+    "The Lamborghini company originally manufactured tractors before venturing into sports car production.",
+    "The term \"horsepower\" was coined by James Watt, the inventor of the steam engine, to help market his invention.",
+    "The world's largest collection of classic cars can be found at the Nethercutt Collection in Sylmar, California.",
+    "The first recorded instance of a car race occurred in 1867 between two steam-powered vehicles in France.",
+    "The Rolls-Royce Phantom has self-righting wheel centers to keep the brand's logo upright when the car is moving.",
+    "The Mercedes-Benz G-Class (G-Wagon) was originally designed as a military vehicle.",
+    "The first car radio was introduced by Chevrolet in 1922.",
+    "The Porsche 911 is one of the few sports cars that has maintained its rear-engine design since its introduction in 1963.",
+    "The average car spends about 95% of its time parked.",
+    "The Toyota Corolla is the best-selling car model of all time, with over 44 million units sold.",
+    "The first hybrid car was the Toyota Prius, introduced in Japan in 1997.",
+    "The most expensive car ever sold at auction is a 1962 Ferrari 250 GTO, which fetched $48.4 million.",
+    "The iconic \"Jeep\" name comes from the phonetic pronunciation of \"G.P.,\" which stands for General Purpose or Government Purpose vehicle."
+];
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -79,6 +104,10 @@ impl EventHandler for Handler {
                 .direct_message(&ctx, CreateMessage::new().content(res))
                 .await
                 .unwrap();
+        } else {
+            let rand_idx = thread_rng().gen_range(0..100);
+            let res = CAR_FACTS[rand_idx % CAR_FACTS.len()];
+            msg.channel_id.say(&ctx.http, res).await.unwrap();
         }
     }
 
