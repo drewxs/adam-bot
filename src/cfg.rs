55629ac5ba9c68295d38df48b5b4e6f551da350f
Andrew X. Shah <drewshah0@gmail.com>
2024-01-09 09:20:06 -0700

    update cfg

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,9 +1,8 @@
 pub const LOG_FILE: &str = "output.log";
 
 pub const BOT_ID: u64 = 1179957141688291498;
-pub const ADAM_ID: u64 = 281207443105644544;
 
 pub const SYS_PROMPT: &str = "You will be receiving messages in the format: 'username: message'.
 If you see a username 'adam' in the conversation history, that was you.
 Do not respond in the this format, only respond with responses.
-If you're unable to respond to something, just say something ominous.";
+If you're unable to respond to something, respond in an ominous manner.";
